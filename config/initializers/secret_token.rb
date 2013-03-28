3778caf9e76f1425a334297e8c7a29b6ef40c24b
Carlos Cheung <cc653@georgetown.edu>
2013-03-27 21:34:14 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Excelsis::Application.config.secret_token = '476e0bf815988959760a6afbe0a867af18d5c6a415b56b93acb12a6c6f19365050ffe24f3a697eb7d0b7b66e8ff427d016f814b9f8884604dec34f37e75f4e11'
